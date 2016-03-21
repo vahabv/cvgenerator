6d1aaf2ca865a5045c5336b30f93e16ac007d4fd
Vahab Valiyev <vahab.v@code.edu.az>
2016-03-21 08:01:40 -0700

    head and footer sections ready

--- index.php
+++ index.php
@@ -9,8 +9,52 @@
 </head>
 <body>
 <div class="mainWrapper col-md-12">
+<div class="verticalAlign"></div>
+<!-- Header bar start -->
+		<div class="container">
+			<div class="logo pull-left">
+				<i class="fa fa-file-text-o"></i><span> <b>RESUME</b>CREATOR</span>
+			</div>
+			<div class="pull-right">
+				<button class="login">Login</button>
+			</div>
+		</div>
+<!-- Header Bar end -->
+<!-- Middle button, text start -->
+		<div class="container">
+			<div class="middleSection">
+				<span>Create Beautiful, Professional Resumes Free</span>
+				<div class="verticalAlign"></div>
+				<div class="createStart">Lets Create Resume</div>
+			</div>
+		</div>
+<!-- Middle button, text end -->
 </div>
-
+<div class="footerWrapper col-md-12">
+	<div class="container">
+		<div class="col-md-2 counter">
+		<span><b>200467</b><br><span>Resumes Created</span></span>
+		</div>
+		<div class="col-md-2 counter">
+		<span><b>320467</b><br><span>Downloads</span></span>
+		</div>
+		<div class="col-md-1"></div>
+		<div class="col-md-4">
+		<ul>
+			<li>Azerbaycanca</li>
+			<li>English</li>
+			<li>Deutshc</li>
+			<li>Espanol</li>
+			<li>Italiano</li>
+			<li>Turkce</li>
+			<li>Portugales</li>
+		</ul>
+		</div>
+		<div class="col-md-3 copyright">
+		<span>Resume Creator © 2016. All rights reserved</span>
+		</div>
+	</div>
+</div>
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
 	
