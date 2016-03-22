dd4613d741002a2aa018e229e913267d9e2307a5
Vahab Valiyev <vahab.v@code.edu.az>
2016-03-22 03:10:54 -0700

    jquery added to select.php

--- select.php
+++ select.php
@@ -14,17 +14,28 @@
 				<span><i class="fa fa-file-text-o"></i> <b> RESUME</b>CREATOR</span>
 			</div>
 			<div class="pull-right">
-				<div class="userArea"><img src="images/resumes.png"> <span> Vahab Valiyev </span></div>
+				<div class="userArea"><img src="images/resumes.png"> <span> Vahab Valiyev </span> <i class="fa fa-angle-down" id="slideDown"></i></div>
+				<div id="userMenu"></div>
 			</div>
 		</div>
 </div>
 <div class="selectTemplate col-md-12 col-xs-12 col-sm-12 col-lg-12">
 	<p>Select your template </p>
-	<div class="container templates">
-		<div class="template col-md-3"></div>
-		<div class="template col-md-3"></div>
-		<div class="template col-md-3"></div>
+	<div class="container templates col-xs-12 col-md-12">
+		<div class="template col-md-3 col-xs-12 col-sm-12"></div>
+		<div class="template col-md-3 col-xs-12 col-sm-12"></div>
+		<div class="template col-md-3 col-xs-12 col-sm-12"></div>
 	</div>
 </div>
+<script type="text/javascript">
+	$("#slideDown").click(function(){
+		if($("#userMenu").is(":visible")){
+			$(".userArea" ).animate({"border-radius":"20px"}, 50);
+		}else{
+			$(".userArea" ).animate({"border-radius":"5px"}, 50);
+		}
+		$("#userMenu").toggle("fast");
+	});
+</script>
 </body>
 </html>
