796fe0ae74289289b879219c80931b2f05a2e484
Vahab Valiyev <vahab.v@code.edu.az>
2016-03-22 02:18:01 -0700

    select.php added to project

---
+++ select.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link rel="stylesheet" href="css/bootstrap.min.css">
+	 <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	 <link rel="stylesheet" type="text/css" href="css/style.css">
+ 	 <script src="jquery-2.2.1.js"></script>
+</head>
+<body>
+<div class="navigationBar col-md-12 col-xs-12 col-sm-12">
+		<div class="container">
+			<div class="logo pull-left col-md-4 col-xs-12 col-sm-12">
+				<span><i class="fa fa-file-text-o"></i> <b> RESUME</b>CREATOR</span>
+			</div>
+			<div class="pull-right">
+				<div class="userArea"><img src="images/resumes.png"> <span> Vahab Valiyev </span></div>
+			</div>
+		</div>
+</div>
+<div class="selectTemplate col-md-12 col-xs-12 col-sm-12 col-lg-12">
+	<p>Select your template </p>
+	<div class="container templates">
+		<div class="template col-md-3"></div>
+		<div class="template col-md-3"></div>
+		<div class="template col-md-3"></div>
+	</div>
+</div>
+</body>
+</html>
